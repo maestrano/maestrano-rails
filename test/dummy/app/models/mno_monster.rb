05f7d4bab5d16000443e8d0857877fdc0eed589f
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-05-27 01:09:10 +1000

    configured model for mno

--- test/dummy/app/models/mno_monster.rb
+++ test/dummy/app/models/mno_monster.rb
@@ -1,3 +1,6 @@
 class MnoMonster < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :provider, :uid
+  maestrano_user_via :provider, :uid
+  
+  attr_accessible :email, :first_name, :last_name
+  attr_protected :provider, :uid
 end
