62750938c809d7a05d50ea13d38b0739b563ea56
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-05-27 23:50:23 +1000

    reorganised specs

--- test/models/maestrano_user_via_test.rb
+++ test/models/maestrano_user_via_test.rb
@@ -2,34 +2,11 @@ require 'test_helper'
  
 class MaestranoUserViaTest < ActiveSupport::TestCase
   context "class methods" do
-    should "have the right field definition" do
-      assert_equal "provider", MnoMonster.maestrano_options[:provider]
-      assert_equal "uid", MnoMonster.maestrano_options[:uid]
-      assert MnoMonster.maestrano_options[:mapping].is_a?(Proc)
-    end
-  end
-  
-  context "instance methods" do
-    context "maestrano?" do
-      should "return true if provider is maestrano and uid not null" do
-        m = MnoMonster.new
-        m.provider = 'maestrano'
-        m.uid = 'usr-1'
-        assert m.maestrano?
-      end
-      
-      should "return false if provider is something else" do
-        m = MnoMonster.new
-        m.provider = 'somethingelse'
-        m.uid = 'usr-1'
-        assert !m.maestrano?
-      end
-      
-      should "return false if uid is blank" do
-        m = MnoMonster.new
-        m.provider = 'maestrano'
-        m.uid = ''
-        assert !m.maestrano?
+    context "maestrano_*_via" do
+      should "have the right field definition" do
+        assert_equal "provider", MnoMonster.maestrano_options[:provider]
+        assert_equal "uid", MnoMonster.maestrano_options[:uid]
+        assert MnoMonster.maestrano_options[:mapping].is_a?(Proc)
       end
     end
     
@@ -60,6 +37,31 @@ class MaestranoUserViaTest < ActiveSupport::TestCase
         assert_equal result_hash[:info][:email], m.email
         assert_equal result_hash[:info][:first_name], m.first_name
         assert_equal result_hash[:info][:last_name], m.last_name
+      end
+    end
+  end
+  
+  context "instance methods" do
+    context "maestrano?" do
+      should "return true if provider is maestrano and uid not null" do
+        m = MnoMonster.new
+        m.provider = 'maestrano'
+        m.uid = 'usr-1'
+        assert m.maestrano?
+      end
+      
+      should "return false if provider is something else" do
+        m = MnoMonster.new
+        m.provider = 'somethingelse'
+        m.uid = 'usr-1'
+        assert !m.maestrano?
+      end
+      
+      should "return false if uid is blank" do
+        m = MnoMonster.new
+        m.provider = 'maestrano'
+        m.uid = ''
+        assert !m.maestrano?
       end
     end
     
