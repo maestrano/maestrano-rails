826aa055aea3530faa027eb6bd40ecdb49df466c
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-05-27 22:25:55 +1000

    added doco

--- lib/generators/maestrano/templates/saml_controller.rb
+++ lib/generators/maestrano/templates/saml_controller.rb
@@ -1,7 +1,12 @@
 class Maestrano::Auth::SamlController < ApplicationController
   
-  def init
-  end
+  # == GET '/maestrano/auth/saml/init'
+  # == Action automatically handled by parent controller
+  # Initialize the SAML request and redirects the
+  # user to Maestrano.
+  # def init
+  #   redirect_to Maestrano::Saml::Request.new(params,session).redirect_url
+  # end
   
   def consume
   end
