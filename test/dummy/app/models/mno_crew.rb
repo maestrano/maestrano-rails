27831eac1a0fbd701b57e3bfc1dc77e5a153e3c4
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-05-27 23:48:29 +1000

    changed crew to use block style

--- test/dummy/app/models/mno_crew.rb
+++ test/dummy/app/models/mno_crew.rb
@@ -1,5 +1,7 @@
 class MnoCrew < ActiveRecord::Base
-  maestrano_group_via :provider, :uid
+  maestrano_group_via :provider, :uid do |group,maestrano|
+    group.name = maestrano.company_name || "Your Group"
+  end
   
   attr_accessible :name
   attr_protected :provider, :uid
