a92c1931543705a024c1ef012dbd85834614fe9e
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-05-28 15:36:04 +1000

    added saml controller helps

--- app/controllers/maestrano/rails/saml_base_controller.rb
+++ app/controllers/maestrano/rails/saml_base_controller.rb
@@ -1,8 +1,34 @@
 class Maestrano::Rails::SamlBaseController < ApplicationController
+  before_filter :process_saml_response, only: [:consume]
   
   # Initialize the SAML request and redirects the
   # user to Maestrano
   def init
     redirect_to Maestrano::Saml::Request.new(params,session).redirect_url
+  end
+  
+  #===================================
+  # Helper methods
+  #===================================
+  def process_saml_response
+    if params[:SAMLResponse]
+      @saml_response = Maestrano::Saml::Response.new(params[:SAMLResponse])
+      if @saml_response.is_valid?
+        @user_auth_hash = Maestrano::SSO::BaseUser.new(@saml_response).to_hash
+        @group_auth_hash = Maestrano::SSO::BaseGroup.new(@saml_response).to_hash
+      end
+    end
+  end
+  
+  def saml_response
+    @saml_response
+  end
+  
+  def user_auth_hash
+    @user_auth_hash
+  end
+  
+  def group_auth_hash
+    @group_auth_hash
   end
 end
--- test/controllers/saml_controller_test.rb
+++ test/controllers/saml_controller_test.rb
@@ -5,13 +5,54 @@ class SamlBaseControllerTest < ActionController::TestCase
   
   context "init phase" do
     should "create a saml request using params and session and redirect the user" do
-      r = mock('saml_request_instance')
+      req = mock('saml_request_instance')
       req_params = {'controller' => 'maestrano/auth/saml', 'action' => 'init', 'a_param' => 'value'}
       
-      r.stubs(:redirect_url).returns("http://idpprovider.com?r=request")
-      Maestrano::Saml::Request.stubs(:new).with(req_params,@request.session).returns(r)
+      req.stubs(:redirect_url).returns("http://idpprovider.com?r=request")
+      Maestrano::Saml::Request.stubs(:new).with(req_params,@request.session).returns(req)
       get :init, a_param: 'value'
-      assert_redirected_to r.redirect_url
+      assert_redirected_to req.redirect_url
+    end
+  end
+  
+  context "consume phase" do
+    setup do
+      @saml_attr = {
+        'mno_session'          => 'f54sd54fd64fs5df4s3d48gf2',
+        'mno_session_recheck'  => Time.now.utc.iso8601,
+        'group_uid'            => 'cld-1',
+        'group_end_free_trial' => Time.now.utc.iso8601,
+        'group_role'           => 'Admin',
+        'uid'                  => "usr-1",
+        'virtual_uid'          => "usr-1.cld-1",
+        'email'                => "j.doe@doecorp.com",
+        'virtual_email'        => "usr-1.cld-1@mail.maestrano.com",
+        'name'                 => "John",
+        "surname"              => "Doe",
+        "country"              => "AU",
+        "company_name"         => "DoeCorp"
+      }
+      @saml_resp = mock('saml_response')
+      @saml_resp.stubs(:attributes).returns(@saml_attr)
+      @saml_resp.stubs(:is_valid?).returns(true)
+      Maestrano::Saml::Response.stubs(:new).returns(@saml_resp)
+    end
+    
+    should "set a saml_request in scope" do
+      post :consume, SAMLResponse: "g45ad5v40xc4b3fd478"
+      assert_equal @saml_resp, @controller.saml_response
+    end
+    
+    should "set the user_auth_hash in scope" do
+      post :consume, SAMLResponse: "g45ad5v40xc4b3fd478"
+      expected_hash = Maestrano::SSO::BaseUser.new(@saml_resp).to_hash
+      assert_equal expected_hash, @controller.user_auth_hash
+    end
+    
+    should "set the group_auth_hash in scope" do
+      post :consume, SAMLResponse: "g45ad5v40xc4b3fd478"
+      expected_hash = Maestrano::SSO::BaseGroup.new(@saml_resp).to_hash
+      assert_equal expected_hash, @controller.group_auth_hash
     end
   end
   
