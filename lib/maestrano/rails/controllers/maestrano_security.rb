7c016dd8b5cfa563b15bded17201f25f1987c8a3
bruno <bruno.chauvet@maestrano.com>
2016-09-20 19:50:06 +1000

    use preset from session

--- lib/maestrano/rails/controllers/maestrano_security.rb
+++ lib/maestrano/rails/controllers/maestrano_security.rb
@@ -17,14 +17,26 @@ module Maestrano
         # its validity. If not valid anymore the filter
         # triggers a Maestrano SSO handshake
         def verify_maestrano_session
-          if Maestrano.param(:sso_enabled)
+          if Maestrano[session_preset].param(:sso_enabled)
             unless controller_name == 'saml' && ['init','consume'].include?(action_name)
-              if !Maestrano::SSO::Session.new(session).valid?(if_session:true)
-                redirect_to Maestrano::SSO.init_url
+              if !Maestrano::SSO::Session[session_preset].new(session).valid?(if_session:true)
+                redirect_to Maestrano::SSO[session_preset].init_url
               end
             end
           end
           true
+        end
+
+        def session_preset
+          @session_preset ||= begin
+            mno_session = (self.session[:maestrano] || self.session['maestrano'])
+            if mno_session
+              decrypted_session = JSON.parse(Base64.decode64(mno_session))
+              decrypted_session['preset']
+            else
+              nil
+            end
+          end
         end
       end
     end
