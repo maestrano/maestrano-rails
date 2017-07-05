aac7fa6923f94f6be90f15da4f1a7bc6933d6d58
BrunoChauvet <bruno.chauvet@maestrano.com>
2017-07-05 15:45:23 +1000

    fix passing tenant parameter in SSO base user

--- app/controllers/maestrano/rails/saml_base_controller.rb
+++ app/controllers/maestrano/rails/saml_base_controller.rb
@@ -2,14 +2,14 @@ class Maestrano::Rails::SamlBaseController < ApplicationController
   attr_reader :saml_response, :user_auth_hash, :group_auth_hash, :user_group_rel_hash
   protect_from_forgery :except => [:consume]
   around_filter :saml_response_transaction, only: [:consume]
-  
+
   # GET /maestrano/auth/saml/init/:tenant
   #
   # Initialize the SAML request and redirects the user to Maestrano
   def init
     redirect_to Maestrano::Saml::Request[params[:tenant]].new(params, session).redirect_url
   end
-  
+
   #===================================
   # Helper methods
   #===================================
@@ -28,12 +28,12 @@ class Maestrano::Rails::SamlBaseController < ApplicationController
       end
     end
   end
-  
+
   def process_saml_response
     if params[:SAMLResponse]
       @saml_response = Maestrano::Saml::Response[params[:tenant]].new(params[:SAMLResponse])
       if @saml_response.validate!
-        @user_auth_hash = Maestrano::SSO::BaseUser.new(@saml_response).to_hash
+        @user_auth_hash = Maestrano::SSO::BaseUser[params[:tenant]].new(@saml_response).to_hash
         @group_auth_hash = Maestrano::SSO::BaseGroup.new(@saml_response).to_hash
         @user_group_rel_hash = Maestrano::SSO::BaseMembership.new(@saml_response).to_hash
       end
